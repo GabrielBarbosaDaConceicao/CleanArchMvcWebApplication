29b6beeb1dc5925c5fc6a9e7cf3401bd28ad7e4b
Gabriel Conceição <gabriel.bc.dev@hotmail.com>
2026-02-24 10:42:10 -0300

    Implementação das paginas de criação e edição para categories, ajustes no código.

--- src/CleanArchMvc.WebUI/Controllers/CategoriesController.cs
+++ src/CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -1,5 +1,7 @@
-﻿using CleanArchMvc.Application.Interfaces;
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -19,6 +21,59 @@ namespace CleanArchMvc.WebUI.Controllers
             var categoriesDto = await _categoryService.GetCategories();
 
             return View(categoriesDto.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryDto categoryDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _categoryService.Add(categoryDto);
+                return RedirectToAction(nameof(Index));
+            }
+                
+            return View(categoryDto);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                 return View("Error");
+
+            var categoryDto = await _categoryService.GetById(id);
+
+            if (categoryDto == null)
+                return View("Error");
+
+            return View(categoryDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(CategoryDto categoryDto)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _categoryService.Update(categoryDto);
+                    
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+                
+            return View(categoryDto);
         }
     }
 }
